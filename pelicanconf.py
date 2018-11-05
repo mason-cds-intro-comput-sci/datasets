edf359763e127ac5c1f84a5ffedcb982c852424b
James K. Glasbrenner <jglasbr2@gmu.edu>
2018-11-05 11:21:22 -0500

    Initial commit * CC BY-SA 4.0 license * Basic README file * Pelican files for generating content to serve * Build rules and configuration files

---
+++ pelicanconf.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+# GENERAL
+AUTHOR = ""
+SITENAME = "Datasets for CDS-101"
+SITEURL = ""
+SLACK_URL = ""
+GITHUB_URL = "https://github.com/mason-cds-intro-comput-sci/datasets"
+UNIVERSITYURL = ""
+TIMEZONE = "America/New_York"
+DEFAULT_LANG = "en"
+
+# PROCESSING
+CACHE_CONTENT = False
+READERS = {
+    "html": None,
+    "yaml": None,
+}
+
+# THEME
+THEME = ""
+THEME_STATIC_PATHS = []
+FRONT_PAGE_STYLE = ""
+TYPOGRIFY = True
+HIGHLIGHT_JS_STYLE = "default"
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
+
+# Define paths and urls
+PATH = "content"
+STATIC_PATHS = ["."]
+PAGE_PATHS = []
+PAGE_URL = ""
+PAGE_SAVE_AS = ""
+ARTICLE_URL = ""
+ARTICLE_SAVE_AS = ""
+AUTHOR_URL = ""
+AUTHOR_SAVE_AS = ""
+DRAFT_URL = ""
+DRAFT_SAVE_AS = ""
+CATEGORY_URL = ""
+CATEGORY_SAVE_AS = ""
+TAG_URL = ""
+TAG_SAVE_AS = ""
+ARCHIVES_SAVE_AS = ""
+CATEGORIES_SAVE_AS = ""
+TAGS_SAVE_AS = ""
+INDEX_SAVE_AS = ""
+DIRECT_TEMPLATES = []
+PAGINATED_DIRECT_TEMPLATES = []
+TEMPLATE_PAGES = {}
+SUMMARY_MAX_LENGTH = 50
+
+# Menu items
+MENUITEMS = []
+
+# Plugins
+PLUGIN_PATHS = []
+PLUGINS = []
---
+++ publishconf.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+# This file is only used if you use `make publish` or
+# explicitly specify it as your config file.
+
+import os
+import sys
+sys.path.append(os.curdir)
+
+from pelicanconf import *
+
+SITEURL = "http://data.cds101.com"
+RELATIVE_URLS = False
+
+FEED_ALL_ATOM = None
+
+DELETE_OUTPUT_DIRECTORY = True
